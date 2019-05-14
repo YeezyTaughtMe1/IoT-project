01317f0be41b6108ddd84f40979982e93a7424b9
varun <varun.vijay@student.uts.edu.au>
2019-05-14 17:50:59 +1000

    hello

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'stylesheets')));
-app.use(express.static(path.join(__dirname, 'vendor')));
+app.use(express.static(__dirname + '/public/stylesheets'));
+
 
 app.use(logger('dev'));
 app.use(express.json());
