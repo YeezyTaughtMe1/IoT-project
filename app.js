e4ead047182f1f56402ab5846d12b576fea8b208
varun <varun.vijay@student.uts.edu.au>
2019-05-17 16:44:16 +1000

    mongo

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public/stylesheets/'));
+//app.use(express.static(path.join(__dirname + 'public/stylesheets')));
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname + 'public/stylesheets')));
+app.use(express.static(__dirname + '../public/stylesheets/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
