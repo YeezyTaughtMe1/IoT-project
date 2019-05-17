98167dfd9de79647c2d7a0cb6a98e8d748466ad3
varun <varun.vijay@student.uts.edu.au>
2019-05-17 17:14:48 +1000

    commit

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + "./public/stylesheets/"));
+app.use(express.static(__dirname + "/public/stylesheets/"));
+
+app.get('views/index.html', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
