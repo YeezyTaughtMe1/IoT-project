974b14d5da316bcd3fbc2c4072d3479d3563d1ee
varun <varun.vijay@student.uts.edu.au>
2019-05-17 15:54:18 +1000

    impleted mongodb

--- insert.js
+++ insert.js
@@ -1,13 +1,15 @@
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://heroku_rkk9fh3h:9ni9okpduof7mnqmfndv242cok@ds135456.mlab.com:35456/heroku_rkk9fh3h";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("heroku_rkk9fh3h");
-  var myobj = { status: "on", why: "app turned pi on" };
-  dbo.collection("App").insertOne(myobj, function(err, res) {
+function addToDB(MongoClient, url){
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log(res);
-    db.close();
+    var dbo = db.db("heroku_rkk9fh3h");
+    var myobj = { status: "on", why: "app turned pi on" };
+    dbo.collection("App").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      db.close();
+    });
   });
-});
+}
