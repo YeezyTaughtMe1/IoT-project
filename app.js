0fc1eaf78f9a2447320525d47446ec302a06b263
varun <varun.vijay@student.uts.edu.au>
2019-05-17 17:51:10 +1000

    appp

--- app.js
+++ app.js
@@ -21,9 +21,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set(express.static(__dirname + 'public'));
-
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
