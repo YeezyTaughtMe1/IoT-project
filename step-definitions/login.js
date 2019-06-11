ea96c25d579d7e288baef30cce1cea666c61f183
Henry Dinh <HenryDinh@Henrys-Air-4.modem>
2019-06-11 17:49:36 +1000

    Feature Update

--- step-definitions/login.js
+++ step-definitions/login.js
@@ -7,29 +7,41 @@ module.exports = function () {
         
       });
 
-    this.Then(/^User logs in using valid email$/, function() {
+    this.Then(/^User logs in using an valid email$/, function() {
 
         driver.findElement(by.id('email_field')).sendKeys('henrydinh@uts.edu.au');
         
     });
 
-    this.Then(/^User logs in using valid password$/, function() {
+    this.Then(/^User logs in using an valid password$/, function() {
 
         driver.findElement(by.id('password_field')).sendKeys('password');
 
         driver.findElement(by.id('login_button')).click();
     });
 
-    this.Then(/^User is taken to the dashboard$/, function() {
+    this.Then(/^User logs in using an invalid email$/, function() {
+
+        driver.findElement(by.id('email_field')).sendKeys('georges@uts.edu.au');
         
-            // return the promise of an element to the following then.
-            driver.findElement(by.id('switchLEDOn')).click();
-  
     });
 
-//    this.Then(/^User remains on the login screen$/, function() {
+    this.Then(/^User logs in using an invalid password$/, function() {
 
- //   });
+        driver.findElement(by.id('password_field')).sendKeys('kebabshop');
+
+        driver.findElement(by.id('login_button')).click();
+    });
+
+    this.Then(/^User is taken to the dashboard$/, function() {
+        
+        
+    });
+
+    this.Then(/^User remains on the login screen$/, function() {
+        
+        driver.findElement(by.id('login_button')).click();
+   });
 
 
     
